41c77aa5cd8a541411c0752a2c6e8bcea1ce79ad
Asbel Apaza <asbel.elmec@gmail.com>
2020-12-05 16:00:44 -0400

    ADD by iiotatlas footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import HookMqtt from './components/Hook'
 import './App.css';
 
+const myStyle = {
+  fontSize: 10,
+}
+
 function App() {
   return (
     <div className="App">
       <HookMqtt />
+
+      <p style= { myStyle } > by Atlas Automation </p>
     </div>
   );
 }
